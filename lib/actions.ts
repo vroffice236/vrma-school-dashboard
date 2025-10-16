aff602a02102ca2f0eb0a89d41e29817def41e50
vroffice236 <office@vedicroots.ca>
2025-10-15 22:43:37 -0400

    Added amin route & Actions file updated
    Commented the actions code to let the deployment work

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,17 +5,17 @@ import { ChildSchema } from "./validationSchemas";
 
 export const createStudent = async (currentState: {success:boolean, error: boolean}, data:ChildSchema)=>{
     console.log(data + "in the server action");
-    try {
-        await prisma.child.create({
-            data: { // if it shows error, it is due to missing fields, which will be added later, remove if already added!
-                firstName:data.firstName,
-            },
-        })
+    // try {
+    //     await prisma.child.create({
+    //         data: { // if it shows error, it is due to missing fields, which will be added later, remove if already added!
+    //             firstName:data.firstName,
+    //         },
+    //     })
 
-        return {success:true, error:false}
-    }
-    catch (err) {
-        console.log(err);
-        return {success: false, error:true}
-    }
+    //     return {success:true, error:false}
+    // }
+    // catch (err) {
+    //     console.log(err);
+    //     return {success: false, error:true}
+    // }
 }
